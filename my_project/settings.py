aafb26b714b16fb63978b13badc4d72cac66f03a
tlyko <tlyko94@gmail.com>
2019-01-13 19:19:45 +0100

    update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),  # Here you tell django to look for a folder named 'assets'
 ]
 
-STATIC_ROOT = '/mnt/e/Pyhon/Static/django_ticket_database/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DISABLE_COLLECTSTATIC = 1
 
